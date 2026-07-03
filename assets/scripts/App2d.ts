9266e174794e02cf3462ec15a233c75d37f1a127
jun <jiangjun@dreaminglife.cn>
2026-07-03 19:18:51 +0800

    co

--- assets/scripts/App2d.ts
+++ assets/scripts/App2d.ts
@@ -1,14 +1,33 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Sprite } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('App2d')
 export class App2d extends Component {
-    start() {
 
+    @property(Node)
+    hitNode: Node = null;
+
+    private _hitTime: number = 0;
+    private _hitDuration: number = 0.3;
+
+    onEnable() {
+        this.node.on(Node.EventType.TOUCH_END, this.onHit, this);
     }
 
-    update(deltaTime: number) {
-        
+    onDisable() {
+        this.node.off(Node.EventType.TOUCH_END, this.onHit, this);
+    }
+
+    onHit() {
+        this._hitTime = 0;
+    }
+
+    protected update(dt: number): void {
+        if (this._hitTime < this._hitDuration) {
+            this._hitTime += dt;
+            this.hitNode.getComponent(Sprite).material.setProperty('hitTime', this._hitTime);
+        }
     }
 }
-
+
+
